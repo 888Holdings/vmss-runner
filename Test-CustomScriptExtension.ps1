ad17416c8d8722d57d75e77985db45bee8f39e1c
Daniel Stefanescu <156472324+danielste888@users.noreply.github.com>
2025-01-23 09:43:25 +0200

    Update Test-CustomScriptExtension.ps1
    Signed-off-by: Daniel Stefanescu <156472324+danielste888@users.noreply.github.com>

--- Test-CustomScriptExtension.ps1
+++ Test-CustomScriptExtension.ps1
@@ -1,5 +1,23 @@
+param (
+    [string]$folder,
+    [string]$pat
+    
+)
 Start-Transcript -Path "$env:SystemRoot\Temp\PowerShell_transcript.$($env:COMPUTERNAME).$(Get-Date ((Get-Date).ToUniversalTime()) -f yyyyMMddHHmmss).txt" -IncludeInvocationHeader
 Set-PSDebug -Trace 2
 Write-Output "Write-Output output from 888: $($MyInvocation.MyCommand.Name)"
+New-Item -Path $folder -Type Directory
+Set-Location $folder
+Write-Output "$($pat)"
+Write-Output "$($folder)"
+$headers = @{
+    Accept                 = "application/vnd.github+json"
+    Authorization          = "Bearer $pat"
+    "X-GitHub-Api-Version" = "2022-11-28"
+}
+$regTokenUrl = "https://api.github.com/orgs/$($org)/actions/runners/registration-token"
+$r = Invoke-RestMethod -Method Post -Uri $regTokenUrl -Headers $headers
+$token = $r.token
+Write-Output "$($r.token)"
 exit 2
 Stop-Transcript
