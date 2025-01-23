f27b4613ef034be927fc4aa37f76162ce9ba5de4
Daniel Stefanescu <156472324+danielste888@users.noreply.github.com>
2025-01-23 13:15:51 +0200

    Update Test-CustomScriptExtension.ps1
    Signed-off-by: Daniel Stefanescu <156472324+danielste888@users.noreply.github.com>

--- Test-CustomScriptExtension.ps1
+++ Test-CustomScriptExtension.ps1
@@ -18,9 +18,13 @@ $headers = @{
     "X-GitHub-Api-Version" = "2022-11-28"
 }
 $regTokenUrl = "https://api.github.com/orgs/$($org)/actions/runners/registration-token"
-Write-Output "$($regTokenUrl)"
+Write-Output "runner reg url: $($regTokenUrl)"
 $r = Invoke-RestMethod -Method Post -Uri $regTokenUrl -Headers $headers
 $token = $r.token
-Write-Output "$($r.token)"
+Write-Output "token: $($r.token)"
+$currentDateTime = Get-Date
+$runnerName = $currentDateTime.ToString("yyyyMMdd-HHmmss")
+Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-win-x64-2.321.0.zip -OutFile actions-runner-win-x64-2.321.0.zip -Verbose
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.321.0.zip", "$PWD")
 exit 2
 Stop-Transcript
